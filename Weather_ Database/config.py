0ced85c84b8a527182b6fa1cb70b326d5f194578
Your Name <you@example.com>
2022-01-11 21:38:00 -0500

    Adding Challenge file

--- Weather_ Database/config.py
+++ Weather_ Database/config.py
@@ -1,7 +1,7 @@
 g_key="AIzaSyBA9uvzoku7976lyRUAxmDI9KVr2pDdl_U"
 
 
-#weather_api_key = "6c4b8c45157cffb506a8fb74d93ce2b1"
+weather_api_key ="6c4b8c45157cffb506a8fb74d93ce2b1"
 
        
 #AIzaSyCzdqqhpnsbTdeCQpKCeYOS1zD1siM_MKE
